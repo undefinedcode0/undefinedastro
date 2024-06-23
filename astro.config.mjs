92a11cb3f2c2af0bfb3e08403bf5a6150e8e29d0
undefinedcode <chrismilla630@outlook.com>
2024-06-23 18:02:48 -0500

    q

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import rename from 'astro-rename';
 
 export default defineConfig({
   css: ['src/styles/app.css, src/styles/icons.css'],
-  integrations: [svelte()],
-  output: 'static',
-  integrations: [
-    rename(),
-    // compress()
-  ],
+  integrations: [svelte()]
 });
