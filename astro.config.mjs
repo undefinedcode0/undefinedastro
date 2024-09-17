87343141b51b911ab2c8e69701d8a8063633c0e1
Anoncyn <quantum3556@gmail.com>
2024-09-16 21:22:51 -0500

    switch to SCSS; added background information on the 'skill-container's. look at ../src/index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
 export default defineConfig({
-  css: ['src/styles/app.css, src/styles/icons.css'],
+  css: ['src/styles/app.scss', 'src/styles/icons.scss'],
   integrations: [svelte()]
 });
